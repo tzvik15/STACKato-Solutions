6cf5b8cdc6ec71a3d805981e5eb3b08b69dbc5ab
tzvik15 <tzvik15@yahoo.com>
2020-02-20 11:24:15 -0700

    Updates

---
+++ precache-manifest.a2543cba6ae00ada72fba3232b0f963f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa45fccffee80ee0918ce03b63b7740",
+    "url": "/React-Portfolio-2/index.html"
+  },
+  {
+    "revision": "55d82cb5155b1463d1e0",
+    "url": "/React-Portfolio-2/static/css/main.14a7ff08.chunk.css"
+  },
+  {
+    "revision": "9818108dd11f229fb0c5",
+    "url": "/React-Portfolio-2/static/js/2.b6f9d277.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-Portfolio-2/static/js/2.b6f9d277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d82cb5155b1463d1e0",
+    "url": "/React-Portfolio-2/static/js/main.f8a38883.chunk.js"
+  },
+  {
+    "revision": "8e442e9b28423994ef88",
+    "url": "/React-Portfolio-2/static/js/runtime-main.e9e7fc40.js"
+  },
+  {
+    "revision": "195d12845ea1bb623979dddded3a4650",
+    "url": "/React-Portfolio-2/static/media/BurgerFull.195d1284.jpg"
+  },
+  {
+    "revision": "fdf27f87e4d9bc662bd0e65f3f6e2f93",
+    "url": "/React-Portfolio-2/static/media/bassoon2.fdf27f87.jpg"
+  },
+  {
+    "revision": "1f2fea15de9e7d8c2b7a509c527ffbc6",
+    "url": "/React-Portfolio-2/static/media/bonez2.1f2fea15.jpg"
+  },
+  {
+    "revision": "c988c17ac89324d615e68959a52fb5bd",
+    "url": "/React-Portfolio-2/static/media/bonezfull.c988c17a.jpg"
+  },
+  {
+    "revision": "dfa1f039a8ddbfb8f2bd103c1795f931",
+    "url": "/React-Portfolio-2/static/media/flintzFull.dfa1f039.jpg"
+  },
+  {
+    "revision": "9b0f8cb0d741c2870f31f7ec73411933",
+    "url": "/React-Portfolio-2/static/media/god.9b0f8cb0.jpg"
+  },
+  {
+    "revision": "9092b4daa4eed0c38e0ef95cfffb985b",
+    "url": "/React-Portfolio-2/static/media/good.9092b4da.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio-2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3c0a9f3dd99ff59a23a0b34c4ab6a7e1",
+    "url": "/React-Portfolio-2/static/media/python.3c0a9f3d.jpg"
+  },
+  {
+    "revision": "705a9e567326ec3363af65000d84ebb3",
+    "url": "/React-Portfolio-2/static/media/restFull.705a9e56.jpg"
+  },
+  {
+    "revision": "fcb1692c32e9335e653f644cfb13d144",
+    "url": "/React-Portfolio-2/static/media/symphony.fcb1692c.png"
+  },
+  {
+    "revision": "fc11a526ae11a94332a0ab6cbc65bd95",
+    "url": "/React-Portfolio-2/static/media/weatherFull.fc11a526.jpg"
+  }
+]);
