9b7b3499286b5719b25412e5f199765927b2378a
tzvik15 <tzvik15@yahoo.com>
2022-08-16 08:56:22 -0400

    testing change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 import Navbar from "./componants/navbar/navbar";
 import About from "./pages/about";
 import Contact from "./pages/contact";
-import Portfolio from "./pages/portfolio"
-import {HashRouter, BrowserRouter as Router, Route } from "react-router-dom";
+import Portfolio from "./pages/portfolio";
+import { HashRouter, BrowserRouter as Router, Route } from "react-router-dom";
 import Bonez from "./pages/bonez";
 import Bassoon from "./pages/bassoon";
 import Burger from "./pages/burger";
@@ -15,41 +15,39 @@ import Rest from "./pages/rest";
 import Weather from "./pages/weather";
 import Theory from "./pages/Theory";
 import Keys from "./pages/Keys";
-import LearnKeys from "./componants/theory/keys/learn/Learn"
-import PlayKeys from "./componants/theory/keys/play/Play"
+import LearnKeys from "./componants/theory/keys/learn/Learn";
+import PlayKeys from "./componants/theory/keys/play/Play";
 import Chords from "./pages/Chords";
-import LearnChords from "./componants/theory/chords/learn/Learn"
-import PlayChords from "./componants/theory/chords/play/Play"
+import LearnChords from "./componants/theory/chords/learn/Learn";
+import PlayChords from "./componants/theory/chords/play/Play";
 
 function App() {
   return (
-    <HashRouter basename='/'>
-      
-        <body className="body">
-          <div className="content">
-            <Navbar />
-          </div>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path ="/React-Portfolio-2" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/bonez" component={Bonez} />
-          <Route exact path="/bassoon" component={Bassoon} />
-          <Route exact path="/burger" component={Burger} />
-          <Route exact path="/flintz" component={Flintz} />
-          <Route exact path="/python" component={Python} />
-          <Route exact path="/rest" component={Rest} />
-          <Route exact path="/weather" component={Weather} />
-          <Route exact path="/Theory" component={Theory} />
-          <Route exact path="/Theory/keys" component={Keys} />
-          <Route exact path="/Theory/keys/learn" component={LearnKeys} />
-		 	    <Route exact path="/Theory/keys/play" component={PlayKeys} />
-           <Route exact path="/Theory/chords" component={Chords} />
-          <Route exact path="/Theory/chords/learn" component={LearnChords} />
-		 	    <Route exact path="/Theory/chords/play" component={PlayChords} />
-        </body>
-      
+    <HashRouter basename="/">
+      <body className="body">
+        <div className="content">
+          <Navbar />
+        </div>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/React-Portfolio-2" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/bonez" component={Bonez} />
+        <Route exact path="/bassoon" component={Bassoon} />
+        <Route exact path="/burger" component={Burger} />
+        <Route exact path="/flintz" component={Flintz} />
+        <Route exact path="/python" component={Python} />
+        <Route exact path="/rest" component={Rest} />
+        <Route exact path="/weather" component={Weather} />
+        <Route exact path="/Theory" component={Theory} />
+        <Route exact path="/Theory/keys" component={Keys} />
+        <Route exact path="/Theory/keys/learn" component={LearnKeys} />
+        <Route exact path="/Theory/keys/play" component={PlayKeys} />
+        <Route exact path="/Theory/chords" component={Chords} />
+        <Route exact path="/Theory/chords/learn" component={LearnChords} />
+        <Route exact path="/Theory/chords/play" component={PlayChords} />
+      </body>
     </HashRouter>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,148 +1,149 @@
 import React from "react";
 import good from "../assets/pics/good.jpg";
-import resume from "../assets/docs/Coding Resume.pdf"
+import resume from "../assets/docs/Coding Resume.pdf";
 import { Link } from "react-router-dom";
 
 function About() {
-    function modeFun1() {
-        const modal2 = document.getElementById("tModal");
-    
-        // Get the button that opens the modal
-        const btn = document.getElementById("techModal");
-    
-        // Get the <span> element that closes the modal
-        const span2 = document.getElementsByClassName("close-btn")[0];
-    
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-          modal2.style.display = "block";
-        };
-    
-        // When the user clicks on <span> (x), close the modal
-        span2.onclick = function() {
-          modal2.style.display = "none";
-        };
-    
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target === modal2) {
-            modal2.style.display = "none";
-          }
-        };
+  function modeFun1() {
+    const modal2 = document.getElementById("tModal");
+
+    // Get the button that opens the modal
+    const btn = document.getElementById("techModal");
+
+    // Get the <span> element that closes the modal
+    const span2 = document.getElementsByClassName("close-btn")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+      modal2.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span2.onclick = function () {
+      modal2.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target === modal2) {
+        modal2.style.display = "none";
       }
+    };
+  }
 
-    return (
-        <>
-        <section className="contactbox" id="box">
-          <h2>
-            About Me <hr />
-          </h2>
-          <img src={good} alt="picMe" id="mepic" />
-          <p className="break" id="pOne">
-            Hello everyone, and thank you for visiting my developer site! In
-            this section I will tell you a little about myself, my history,
-            and the work I've done and hoping to do.
-          </p>
-          <p className="break" id="pTwo">
-            Let's start with the basics of my background. My formal studies
-            were in the fields of music and education (I have a doctorate in
-            performance and pedagogy!) I grew up in Israel, where I did my
-            undergrad, and later moved to Los Angeles and finally to Colorado
-            for my graduate work.
-          </p>
-          <p className="break" id="pThree">
-            It was here in Colorado that I learned how important collaboration
-            is to me. In fact, the concept is so meaningful for me that I
-            wrote my doctoral thesis on the subject!
-          </p>
-          <br />
-          <p className="break" id="pFour">
-            I took my first steps as a developer at the University of Denver,
-            where I immedietly began encorporating my thoughts and ideas on
-            collaborative work into my studies, collaborating on projects, and
-            working with my cohort members to help and support each other in
-            our journey as developers.
-          </p>
-          <p className="break" id="pFive">
-            As a developer, I am passionate about using JavaScript to find
-            creative solutions to coding challenges. An example of my JS can
-            be seen in my "Bone'z Beatz" project, where I used JS to connect
-            dynamically between sound files and key strokes, creating a fully
-            custumizable soundboard application. (more about this project{" "}
-            <Link to="/bonez" className="hereLink">HERE</Link>)
-          </p>
-          <p className="break" id="pSix">
-            As my client, I will work together with you, collaborating to
-            realize the projects you envision, providing you with the best
-            quality and support, and the knowledge that the final product will
-            truely be a part and representation of YOU and YOUR needs.
-          </p>
-          <p className="break" id="pSeven">
-            You can see examples of my projects in the{" "}
-            <Link to="/portfolio">PORTFOLIO</Link> section.{" "}
-            <a
-              href="https://github.com/tzvik15"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hereLink"
-            >
-              THIS
-            </a>{" "}
-            is a link to my GitHub page,{" "}
-            <a
-              href="https://www.linkedin.com/in/daniel-nester-aa074a189/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hereLink"
-            >
-              THIS
-            </a>{" "}
-            is a link to my LinkedIn page, and{" "}
-            <a
-              href={resume}
-              download
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hereLink"
-            >
-              HERE
-            </a>{" "}
-            you can download my resume. For my technical skills and
-            proficiencies, click{" "}
-            {/* eslint-disable-next-line */}
-            <a id="techModal" onClick={modeFun1} className="hereLink">
-              HERE.
-            </a>
-          </p>
-          <div id="tModal" className="modal">
-            <div className="modal-content">
-              <span className="close-btn">&times;</span>
-              <p>
-                <h3>Programing Languages Known:</h3> HTML 5, CSS, JavaScript
-                (ES6) <br />
-                <h3>Front End:</h3>React.js (including hooks), REST API
-                queries, 3rd party JS and CSS library integration, deployment
-                of web pages <br />
-                <h3>Back End: </h3>Node.js, Express servers, REST API
-                creation, NPM, Handlebars, Sequelize <br />
-                <h3>Cross Stack: </h3>MERN stack, jQuery, Test Driven
-                Development with Jest, mySQL, Object Oriented Programming,
-                MongoDB and Mongoose, PWA creation (including offline support){" "}
-                <br />
-                <h3>In Development:</h3> ServiceNow <br />
-              </p>
-              <p>
-                To see these skills in use, please click{" "}
-                <Link to="/portfolio" className="hereLink">HERE</Link>.
-              </p>
-               
-            </div>
+  return (
+    <>
+      <section className="contactbox" id="box">
+        <h2>
+          About Me <hr />
+        </h2>
+        <img src={good} alt="picMe" id="mepic" />
+        <p className="break" id="pOne">
+          test Hello everyone, and thank you for visiting my developer site! In this
+          section I will tell you a little about myself, my history, and the
+          work I've done and hoping to do.
+        </p>
+        <p className="break" id="pTwo">
+          Let's start with the basics of my background. My formal studies were
+          in the fields of music and education (I have a doctorate in
+          performance and pedagogy!) I grew up in Israel, where I did my
+          undergrad, and later moved to Los Angeles and finally to Colorado for
+          my graduate work.
+        </p>
+        <p className="break" id="pThree">
+          It was here in Colorado that I learned how important collaboration is
+          to me. In fact, the concept is so meaningful for me that I wrote my
+          doctoral thesis on the subject!
+        </p>
+        <br />
+        <p className="break" id="pFour">
+          I took my first steps as a developer at the University of Denver,
+          where I immedietly began encorporating my thoughts and ideas on
+          collaborative work into my studies, collaborating on projects, and
+          working with my cohort members to help and support each other in our
+          journey as developers.
+        </p>
+        <p className="break" id="pFive">
+          As a developer, I am passionate about using JavaScript to find
+          creative solutions to coding challenges. An example of my JS can be
+          seen in my "Bone'z Beatz" project, where I used JS to connect
+          dynamically between sound files and key strokes, creating a fully
+          custumizable soundboard application. (more about this project{" "}
+          <Link to="/bonez" className="hereLink">
+            HERE
+          </Link>
+          )
+        </p>
+        <p className="break" id="pSix">
+          As my client, I will work together with you, collaborating to realize
+          the projects you envision, providing you with the best quality and
+          support, and the knowledge that the final product will truely be a
+          part and representation of YOU and YOUR needs.
+        </p>
+        <p className="break" id="pSeven">
+          You can see examples of my projects in the{" "}
+          <Link to="/portfolio">PORTFOLIO</Link> section.{" "}
+          <a
+            href="https://github.com/tzvik15"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hereLink"
+          >
+            THIS
+          </a>{" "}
+          is a link to my GitHub page,{" "}
+          <a
+            href="https://www.linkedin.com/in/daniel-nester-aa074a189/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hereLink"
+          >
+            THIS
+          </a>{" "}
+          is a link to my LinkedIn page, and{" "}
+          <a
+            href={resume}
+            download
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hereLink"
+          >
+            HERE
+          </a>{" "}
+          you can download my resume. For my technical skills and proficiencies,
+          click {/* eslint-disable-next-line */}
+          <a id="techModal" onClick={modeFun1} className="hereLink">
+            HERE.
+          </a>
+        </p>
+        <div id="tModal" className="modal">
+          <div className="modal-content">
+            <span className="close-btn">&times;</span>
+            <p>
+              <h3>Programing Languages Known:</h3> HTML 5, CSS, JavaScript (ES6){" "}
+              <br />
+              <h3>Front End:</h3>React.js (including hooks), REST API queries,
+              3rd party JS and CSS library integration, deployment of web pages{" "}
+              <br />
+              <h3>Back End: </h3>Node.js, Express servers, REST API creation,
+              NPM, Handlebars, Sequelize <br />
+              <h3>Cross Stack: </h3>MERN stack, jQuery, Test Driven Development
+              with Jest, mySQL, Object Oriented Programming, MongoDB and
+              Mongoose, PWA creation (including offline support) <br />
+              <h3>In Development:</h3> ServiceNow <br />
+            </p>
+            <p>
+              To see these skills in use, please click{" "}
+              <Link to="/portfolio" className="hereLink">
+                HERE
+              </Link>
+              .
+            </p>
           </div>
-        </section>
-      </>
-
-
-    )
-}   
+        </div>
+      </section>
+    </>
+  );
+}
 
 export default About;
